c4f38359bf0c3bf726ad29e2f74eb3d1c36507ca
Brent Benson <bwbensonjr@gmail.com>
2019-12-29 14:14:38 -0500

    Added MA Congressional calc to tests and README

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ Calculate Partisan Voter Index (PVI)
 
 ## Example
 
+You can call the `pvi.calc_pvi` function on anything that responds with
+with indexes for the required values, e.g., dictionaries or Pandas DataFrames.
+
 ```
 >>> import pvi
 >>> MA_VOTES = {
@@ -22,6 +25,39 @@ Calculate Partisan Voter Index (PVI)
 >>> ma_pvi = pvi.pvi_string(ma_pvi_n)
 >>> ma_pvi
 'D+12'
+```
+
+DataFrame example for Massachusetts Congressional district PVI.
+
+```
+>>> MA_CONG_DIST = pd.DataFrame({
+    'US_House': ['1st Congressional',
+                 '2nd Congressional',
+                 '3rd Congressional',
+                 '4th Congressional',
+                 '5th Congressional',
+                 '6th Congressional',
+                 '7th Congressional',
+                 '8th Congressional',
+                 '9th Congressional'],
+    'Obama_12': [213423, 199549, 189461, 211423, 235984, 212003, 233382, 213364, 212701],
+    'Romney_12': [114339, 133195, 137869, 152699, 119934, 169966, 44275, 150825, 165212],
+    'Clinton_16': [194036, 197492, 202952, 225976, 258908, 224858, 254037, 231356, 205581],
+    'Trump_16': [123953, 129437, 123347, 133705, 95922, 153244, 36018, 131624, 163643],
+})
+>>> MA_CONG_DIST["PVI_N"] = pvi.calc_pvi(MA_CONG_DIST)
+>>> MA_CONG_DIST["PVI"] = MA_CONG_DIST["PVI_N"].map(pvi.pvi_string)
+>>> MA_CONG_DIST.sort_values("PVI_N", ascending=False)
+            US_House  Obama_12  Romney_12  Clinton_16  Trump_16      PVI_N   PVI
+6  7th Congressional    233382      44275      254037     36018  34.321468  D+34
+4  5th Congressional    235984     119934      258908     95922  18.094473  D+18
+0  1st Congressional    213423     114339      194036    123953  11.563200  D+12
+7  8th Congressional    213364     150825      231356    131624   9.622453  D+10
+3  4th Congressional    211423     152699      225976    133705   8.895400   D+9
+1  2nd Congressional    199549     133195      197492    129437   8.652278   D+9
+2  3rd Congressional    189461     137869      202952    123347   8.500776   D+9
+5  6th Congressional    212003     169966      224858    153244   5.941072   D+6
+8  9th Congressional    212701     165212      205581    163643   4.449376   D+4
 ```
 
 ## Run unit tests
--- tests/test_pvi.py
+++ tests/test_pvi.py
@@ -1,5 +1,6 @@
 import unittest
 import pvi
+import pandas as pd
 
 MA_VOTES = {
     "Obama_12": 1921290,
@@ -11,6 +12,44 @@ MA_VOTES = {
 MA_PVI_N = 11.677772318663537
 MA_PVI = "D+12"
 
+MA_CONG_DIST = pd.DataFrame({
+    'US_House': ['1st Congressional',
+                 '2nd Congressional',
+                 '3rd Congressional',
+                 '4th Congressional',
+                 '5th Congressional',
+                 '6th Congressional',
+                 '7th Congressional',
+                 '8th Congressional',
+                 '9th Congressional'],
+    'Obama_12': [213423, 199549, 189461, 211423, 235984, 212003, 233382, 213364, 212701],
+    'Romney_12': [114339, 133195, 137869, 152699, 119934, 169966, 44275, 150825, 165212],
+    'Clinton_16': [194036, 197492, 202952, 225976, 258908, 224858, 254037, 231356, 205581],
+    'Trump_16': [123953, 129437, 123347, 133705, 95922, 153244, 36018, 131624, 163643],
+})
+
+MA_CONG_PVI = pd.DataFrame({
+    'US_House': ['1st Congressional',
+                 '2nd Congressional',
+                 '3rd Congressional',
+                 '4th Congressional',
+                 '5th Congressional',
+                 '6th Congressional',
+                 '7th Congressional',
+                 '8th Congressional',
+                 '9th Congressional'],
+    'PVI': ['D+12', 'D+9', 'D+9', 'D+9', 'D+18', 'D+6', 'D+34', 'D+10', 'D+4'],
+    'PVI_N': [11.563200457363742,
+              8.652278080423725,
+              8.500775526052262,
+              8.895400284891751,
+              18.09447280082861,
+              5.941071639760042,
+              34.32146783259586,
+              9.62245323391302,
+              4.449376000045701],
+})
+
 class PviTests(unittest.TestCase):
 
     def test_ma_pvi(self):
@@ -18,3 +57,14 @@ class PviTests(unittest.TestCase):
         self.assertEqual(ma_pvi_n, MA_PVI_N)
         ma_pvi = pvi.pvi_string(ma_pvi_n)
         self.assertEqual(ma_pvi, MA_PVI)
+
+    def test_ma_cong_pvi(self):
+        MA_CONG_DIST["PVI_N"] = pvi.calc_pvi(MA_CONG_DIST)
+        for i in range(0, len(MA_CONG_DIST)):
+            self.assertAlmostEqual(MA_CONG_DIST.iloc[i]["PVI_N"],
+                                   MA_CONG_PVI.iloc[i]["PVI_N"])
+        MA_CONG_DIST["PVI"] = MA_CONG_DIST["PVI_N"].map(pvi.pvi_string)
+        for i in range(0, len(MA_CONG_DIST)):
+            self.assertEqual(MA_CONG_DIST.iloc[i]["PVI"],
+                             MA_CONG_PVI.iloc[i]["PVI"])
+        
